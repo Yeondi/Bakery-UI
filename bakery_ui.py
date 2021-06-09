9bef410df723f64d29ef85fc0b3fd40a10113aea
Yeondi <51913393+Yeondi@users.noreply.github.com>
2021-06-09 17:07:45 +0900

    Add files via upload

--- bakery_ui.py
+++ bakery_ui.py
@@ -26,10 +26,43 @@ class CustomerView:
 
     def __init_window(self, window):
         window.title("고객: " + self.name)
+        self.sandwich = Label(window,text="샌드위치 (5000원)")
+        self.sandwich.grid(column = 0,row=0)
+        self.input_entry = Entry(window,width=10)
+        self.input_entry.grid(column=1,row=0)
+        self.cake = Label(window, text='케이크 (20000원)')
+        self.cake.grid(column = 0,row=1)
+        self.input_entry2 = Entry(window,width=10)
+        self.input_entry2.grid(column=1,row=1)
+
+        self.btn = Button(window,text="주문하기",command=self.send_order)
+        self.btn.grid(column=0,row=2)
+
+        self.btn.bind("<Return>",self.send_order)
+
         window.geometry('350x200')
 
     def send_order(self):
         order_text = ""
+        try:#문자가 아니면서 0보다 큰 int
+            if int(self.input_entry.get()) > 0 or int(self.input_entry2.get()) > 0:
+                order_text += self.name
+            if int(self.input_entry.get()) > 0 : #숫자가 아닐경우
+                order_text+= ": " + self.sandwich.cget("text") + " " + self.input_entry.get() + "개"
+                if int(self.input_entry2.get()) > 0:
+                    order_text+=", "
+            if int(self.input_entry2.get()) > 0:
+                order_text += ": " + self.cake.cget("text")+ " " + self.input_entry2.get()+"개"
+        except:
+            try:
+                if int(self.input_entry2.get()) > 0:
+                    order_text = self.name
+                if int(self.input_entry2.get()) > 0:
+                    order_text += ": " + self.cake.cget("text")+ " " + self.input_entry2.get()+"개"
+            except:
+                order_text += ""
+
+
         self.bakeryView.add_order(order_text)
 
 
