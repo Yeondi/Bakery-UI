9c39c9f8936edf9449875984891622b4e0f1636d
Yeondi <51913393+Yeondi@users.noreply.github.com>
2021-06-09 17:07:02 +0900

    Add files via upload

---
+++ bakery_ui.py
@@ -0,0 +1,41 @@
+import tkinter
+from tkinter import *
+
+
+class BakeryView:
+    def __init__(self, window):
+        self.__init_window(window)
+
+    def __init_window(self, window):
+        window.title("빵집")
+        window.geometry('400x200')
+        label = Label(window, text='주문내역')
+        label.pack()
+        self.orderText = Text(window)
+        self.orderText.pack()
+
+    def add_order(self, orders):
+        self.orderText.insert(0.0, orders + "\n")
+
+
+class CustomerView:
+    def __init__(self, name, window, bakery_view):
+        self.name = name
+        self.__init_window(window)
+        self.bakeryView = bakery_view
+
+    def __init_window(self, window):
+        window.title("고객: " + self.name)
+        window.geometry('350x200')
+
+    def send_order(self):
+        order_text = ""
+        self.bakeryView.add_order(order_text)
+
+
+if __name__ == '__main__':
+    app = Tk()
+    bakery = BakeryView(app)
+    CustomerView('고객A', Toplevel(app), bakery)
+    CustomerView('고객B', Toplevel(app), bakery)
+    app.mainloop()
